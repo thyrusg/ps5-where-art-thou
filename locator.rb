b171a5bafa01f48afca3e4d7584921f35329f884
Thyrus Gorges <me@thyrusgorges.com>
2021-07-16 10:43:59 -0700

    Pull URLs into their own variables

--- locator.rb
+++ locator.rb
@@ -16,7 +16,8 @@ end
 out_of_stock_statues = ["SOLD_OUT", "Not Available", " Sold Out "]
 in_stock_statues = ["ADD_TO_CART", "Available", "Add to Cart"]
 
-best_buy_body = HTTParty.get("https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149").body
+best_buy_url = "https://www.bestbuy.com/site/sony-playstation-5-console/6426149.p?skuId=6426149"
+best_buy_body = HTTParty.get(best_buy_url).body
 best_buy_document = Oga.parse_html(best_buy_body)
 
 best_buy_add_to_cart_button = best_buy_document.at_css('.add-to-cart-button')
@@ -25,9 +26,11 @@ if in_stock_statues.include?(best_buy_status)
   puts "BB: PS5 in stock order now"
 else
   puts "BB: No PS5s currently available"
+  notify_discord("PS5 Available at Best Buy")
 end
 
-gamestop_body = HTTParty.get("https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html").body
+gamestop_url = "https://www.gamestop.com/video-games/playstation-5/consoles/products/playstation-5/11108140.html"
+gamestop_body = HTTParty.get(gamestop_url).body
 gamestop_document = Oga.parse_html(gamestop_body)
 
 gamestop_add_to_cart_button = gamestop_document.css(".add-to-cart")[0]
@@ -40,7 +43,8 @@ else
   puts "GS: No PS5s currently available"
 end
 
-sony_body = HTTParty.get("https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816").body
+sony_url = "https://direct.playstation.com/en-us/consoles/console/playstation5-console.3005816"
+sony_body = HTTParty.get(sony_url).body
 sony_document = Oga.parse_html(sony_body)
 
 sony_add_to_cart_button = sony_document.at_css(".add-to-cart")
@@ -53,7 +57,8 @@ else
   puts "S: No PS5s currently available"
 end
 
-antonline_body = HTTParty.get("https://www.antonline.com/Sony/Electronics/Gaming_Devices/Gaming_Consoles/1430137").body
+antonline_url = "https://www.antonline.com/Sony/Electronics/Gaming_Devices/Gaming_Consoles/1430137"
+antonline_body = HTTParty.get(antonline_url).body
 antonline_document = Oga.parse_html(antonline_body)
 
 antonline_add_to_cart_button = antonline_document.at_css(".uk-button")
